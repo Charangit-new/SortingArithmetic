35509b7520cd18e193b3053aa70c49154fa6faf4
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-10 01:24:10 +0530

    Added : Store the result in Dictionary

--- arraysort.sh
+++ arraysort.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 echo "Welcome to Sorting Arithmetic Computation Problem "
+declare -A result
 read -p "Enter first input " a
 read -p "Enter second input " b
 read -p "Enter third input " c
@@ -7,9 +8,9 @@ compute1=`echo "$a + $b * $c" | bc -l`
 compute2=`echo "$a * $b + $c" | bc -l`
 compute3=`echo "scale=2;$c + $a / $b" | bc -l`
 compute4=`echo "scale=0;$a % $b + $c" | bc -l`
-echo compute1="$compute1"
-echo compute2="$compute2"
-echo compute3="$compute3"
-echo compute4="$compute4"
+result[1]="$compute1"
+result[2]="$compute2"
+result[3]="$compute3"
+result[4]="$compute4"
 
-
+echo ${result[@]}
