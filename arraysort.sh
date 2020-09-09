099c4b1a21ca86fc8a7ee9735b7a8811b66fa30b
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-10 01:02:08 +0530

    Added c+a/b

--- arraysort.sh
+++ arraysort.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation Problem "
 read -p "Enter first input " a
 read -p "Enter second input " b
 read -p "Enter third input " c
-printf 'compute1=%0.2f' "$(( a + b * c ))"
-printf '\ncompute2=%0.2f' "$(( a * b + c ))"
+compute1=`echo "$a + $b * $c" | bc -l`
+compute2=`echo "$a * $b + $c" | bc -l`
+compute3=`echo "scale=2;$c + $a / $b" | bc -l`
+echo compute1="$compute1"
+echo compute2="$compute2"
+echo compute3="$compute3"
+
